c6e586c5eec5ca617fd80c0277b1b395e9bb0243
Jason Ma <707769862@qq.com>
2021-01-21 16:35:13 +0800

    首页功能开发完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,18 @@
 // 在执行ajax请求前，会先执行ajaxPrefilter这个函数，option获取ajax请求的所有配置项
 $.ajaxPrefilter(function (option) {
-    console.log(option);
     option.url = 'http://api-breakingnews-web.itheima.net/' + option.url
+    // 为需要使用权限验证的接口统一设置headers, 避免每次调用接口都写headers
+    if (option.url.indexOf('/my') != -1) {
+        option.headers = {
+            Authorization: localStorage.getItem('token')
+        }
+    }
+    option.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1. 强制清空 token
+            localStorage.removeItem('token')
+            // 2. 强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+$(function () {
+    getUserInfo();
+
+    // 退出功能
+    $('#loginOut').on('click', function () {
+        layui.layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            localStorage.removeItem('token');
+            location.href = 'login.html';
+            layui.layer.close(index);
+        });
+    })
+})
+
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: 'my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token')
+        // },
+        success: function (res) {
+            if (res.status != 0) {
+                layui.layer.msg(res.message);
+            }
+            renderAvatar(res.data);
+        }
+    })
+}
+
+function renderAvatar(user) {
+    var uname = user.nickname || user.username;
+    $('.text-inner').html('欢迎&nbsp;&nbsp;' + uname);
+    if (user.user_pic != null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide();
+    } else {
+        $('.layui-nav-img').hide();
+        $('.text-avatar').html(uname[0].toUpperCase()).show();
+    }
+}
